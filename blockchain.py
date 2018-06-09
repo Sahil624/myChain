cf2f5da4c640e9c2ba7c658a398eeced6c082b22
Sahil <sahilmangotra1997@gmail.com>
2018-06-09 21:36:26 +0530

    Basic function for hashing made

--- blockchain.py
+++ blockchain.py
@@ -4,39 +4,60 @@ import hashlib
 
 class Block:
 
-	def __init__(self, index, proof, prev_hash, transaction):
+	def __init__(self, index, proof, previous_hash, transactions):
 		self.index = index
 		self.proof = proof
-		self.prev_hash = prev_hash
-		self.transaction = transaction
+		self.previous_hash = previous_hash
+		self.transactions = transactions
 		self.timestamp = time.time()
 
 
 	@property
 	def get_block_hash(self):
-		block_string = '{}{}{}{}{}'.format(self.index, self.proof, self.prev_hash, self.transaction, self.timestamp)
+		block_string = '{}{}{}{}{}'.format(self.index, self.proof, self.previous_hash, self.transactions, self.timestamp)
 		return hashlib.sha256(block_string.encode()).hexdigest()
 
 
 class BlockChain:
 	def __init__(self):
 		self.chain = []
-		self.current_node_transaction = []
+		self.current_node_transactions = []
 		self.create_genesis_node()
 
 	def create_genesis_node(self):
-		pass
+		self.create_new_block(proof = 0, previous_hash = 0)
 
-	def create_new_block(self):
-		pass
+	def create_new_block(self, proof, previous_hash):
+		block = Block(
+				index=len(self.chain),
+				proof=proof,
+				previous_hash=previous_hash,
+				transactions=self.current_node_transactions
+			)
 
-	def create_new_transaction(self):
-		pass
+		self.chain.append(block)
+		self.current_node_transactions = []
+
+		return block
+
+	def create_new_transaction(self ,sender ,reciver ,amount):
+		self.current_node_transactions.append({
+			'sender': sender,
+			'reciver': reciver,
+			'amount': amount
+			})
+
+		return self.get_last_block.index + 1
 
 	@staticmethod
-	def create_proof_of_work(self):
-		pass
+	def create_proof_of_work(prev_proof):
+		proof = prev_proof + 1
+
+		while (proof + prev_proof) % 7 != 0:
+			proof += 1
+
+		return proof
 
 	@property
 	def get_last_block(self):
-		pass
+		return self.chain[-1]
