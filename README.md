87f6293d70ce29d83f7b96a13fc8de07f198b0b0
Sahil <sahilmangotra1997@gmail.com>
2018-06-11 07:41:17 +0000

    commening

--- README.md
+++ README.md
@@ -5,12 +5,15 @@
 ### To run app localy
 
 1 Clone the app
-... ` git clone https://github.com/Sahil624/myChain.git `
+
+` git clone https://github.com/Sahil624/myChain.git `
 
 2 Install all the dependencies
-... ` pip install -r requirements.txt `
+
+ ` pip install -r requirements.txt `
 
 3 Run app by
+
 ``` python
     python blockcahin.py
 ```
--- blockchain.py
+++ blockchain.py
@@ -14,42 +14,65 @@ app = Flask(__name__)
 class Block:
 
 	def __init__(self, index, proof, previous_hash, transactions):
-		self.index = index
-		self.proof = proof
-		self.previous_hash = previous_hash
-		self.transactions = transactions
-		self.timestamp = time.time()
+		self.index = index # index of block in blockchain
+		self.proof = proof # A number made after successful mining process. Block will be created using this proof
+		self.previous_hash = previous_hash # Hash of previous block. maintaing the chain
+		self.transactions = transactions # dictionary of all the transactions queued
+		self.timestamp = time.time() # Current timestamp. when  block is created
 
 
+	# Create a hash of this block for refering 
 	@property
 	def get_block_hash(self):
+		
+		"""
+			This hash makes the blockchain secure. Present hash is calculated using previous hash.
+			If any attacker changes the block whole chain is broken.
+		"""
+		
 		block_string = '{}{}{}{}{}'.format(self.index, self.proof, self.previous_hash, self.transactions, self.timestamp)
 		return hashlib.sha256(block_string.encode()).hexdigest()
 
 
 class BlockChain:
+	
 	def __init__(self):
+		"""
+			When a instance of blockchain is created :- 
+			1) The chain is initialed to empty list.
+			2) There are no transactions in blockchain.
+			3) A genesis block (First block of chain ) is created.
+		"""
 		self.chain = []
 		self.current_node_transactions = []
 		self.create_genesis_node()
 
+	# First block of chain with proof and previous hash 0
 	def create_genesis_node(self):
 		self.create_new_block(proof = 0, previous_hash = 0)
 
 	@property
 	def get_serialized_chain(self):
+		"""
+			returns serialized chain
+		"""
 		return [vars(block) for block in self.chain]
 
 	def create_new_block(self, proof, previous_hash):
+		
+		"""
+			Create new block
+		"""
+		
 		block = Block(
-				index=len(self.chain),
-				proof=proof,
-				previous_hash=previous_hash,
+				index=len(self.chain), # index is length of chain + 1
+				proof=proof, 
+				previous_hash=previous_hash, # Pending transactions are assigned to new Node
 				transactions=self.current_node_transactions
 			)
 
-		self.chain.append(block)
-		self.current_node_transactions = []
+		self.chain.append(block) # New Block is appened to the chain 
+		self.current_node_transactions = [] # As transaction are assigned to new block. So, current txns are reseted
 
 		return block
 
@@ -65,7 +88,9 @@ class BlockChain:
 	@staticmethod
 	def create_proof_of_work(prev_proof):
 		proof = prev_proof + 1
-
+		"""
+			Proof of work algorithm 
+		"""
 		while (proof + prev_proof) % 7 != 0:
 			proof += 1
 
@@ -73,11 +98,16 @@ class BlockChain:
 
 	@property
 	def get_last_block(self):
+		"""
+			return last block of the chain
+		"""
 		return self.chain[-1]
 
 	def mine_block(self, miner_address):
-		# Sender "0" means that this node has mined a new block
-		# For mining the Block(or finding the proof), we must be awarded with some amount(in our case this is 1)
+		"""
+		 Sender "0" means that this node has mined a new block
+		 For mining the Block(or finding the proof), we must be awarded with some amount(in our case this is 1)
+		"""
 		self.create_new_transaction(
 			sender="0",
 			reciver=miner_address,
@@ -95,10 +125,12 @@ class BlockChain:
 		return vars(block) # Return a native Dict type object
 
 
-blockcahin = BlockChain()
+blockchain = BlockChain()
 
+# Create unique address of current node
 node_address = uuid4().hex
 
+# Dummy index function
 @app.route('/')
 def hello_world():
 	return 'Hello World!'
@@ -107,7 +139,7 @@ def hello_world():
 def create_transaction():
 	transaction_data = request.get_json()
 
-	index = blockcahin.create_new_transaction(**transaction_data)
+	index = blockchain.create_new_transaction(**transaction_data)
 
 	response = {
 		'message': 'Transaction has been submitted successfully',
@@ -119,7 +151,7 @@ def create_transaction():
 
 @app.route('/mine',methods = ['GET'])
 def mine():
-	block = blockcahin.mine_block(node_address)
+	block = blockchain.mine_block(node_address)
 
 	response = {
 		'message': 'Data mined successfully',
@@ -131,7 +163,7 @@ def mine():
 @app.route('/full_chain',methods = ['GET'])
 def full_chain():
 	response = {
-		'blockchain': blockcahin.get_serialized_chain
+		'blockchain': blockchain.get_serialized_chain
 	}
 
 	return jsonify(response)
